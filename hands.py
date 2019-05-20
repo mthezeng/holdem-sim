e8e7037f247d8169d874b29c55373de7cd74001f
mzeng7 <mzeng7@berkeley.edu>
2019-05-20 19:49:03 -0400

    implement full house

--- hands.py
+++ hands.py
@@ -43,6 +43,12 @@ class Card:
     def get_suit(self):
         return self.suit
 
+    def get_fullname(self):
+        return self.full_names[self.num]
+
+    def full_name(num):
+        return Card.full_names[num]
+
 class Suit(Enum):
     HEARTS = 'h'
     DIAMONDS = 'd'
@@ -70,42 +76,84 @@ class Hand:
 class StraightFlush(Hand):
     value = 9 # strongest hand in holdem poker
 
-    def __init__(self, high_card):
-        super().__init__(self)
-        self.high_card = high_card
+    def __init__(self, high_num):
+        super().__init__()
+        if not isinstance(high_num, int):
+            raise TypeError("high_num must be an int, not {0}".format(type(high_num)))
+        if not(5 <= high_num <= 13) and high_num != 1:
+            raise ValueError("high card for a straight cannot be {0}".format(high_num))
+        self.high_num = high_num
 
     def __lt__(self, other):
         if isinstance(other, StraightFlush):
-            return self.high_card < other.high_card
+            return self.high_num < other.high_num
         return super()
 
     def __gt__(self, other):
         if isinstance(other, StraightFlush):
-            return self.high_card > other.high_card
+            return self.high_num > other.high_num
         return super()
 
     def __eq__(self, other):
         if super():
-            return self.high_card == other.high_card
+            return self.high_num == other.high_num
         return False
 
+    def __ne__(self, other):
+        return not self.__eq__(self, other)
+
     def __str__(self):
-        return "Straight flush, {0} to {1}".format(self.high_card, self.high_card - 4)
+        if self.high_num == 1:
+            return "Royal flush"
+        return "Straight flush, {0} to {1}".format(
+        Card.full_name(self.high_num),
+        Card.full_name((self.high_num - 4) % 13)
+        )
 
 class FourOfAKind(Hand):
     value = 8
 
-    def __init__(self, quad_card):
-        self.quad_card = quad_card
+    def __init__(self, quad_num, kicker):
+        if not isinstance(quad_num, int):
+            raise TypeError("quad_num must be an int, not {0}".format(type(quad_num)))
+        self.quad_num = quad_num
+        self.kicker = kicker
 
     def __str__(self):
-        return "Four of a kind, ".format(str(self.quad_card))
+        return "Four of a kind, {0}s".format(Card.full_name(self.quad_num))
 
 class FullHouse(Hand):
     value = 7
 
     def __init__(self, set, full):
-        pass
+        if not(isinstance(set, int)) or not(isinstance(full, int)):
+            raise TypeError("set and full must be ints")
+        self.set = set
+        self.full = full
+
+    def __lt__(self, other):
+        if isinstance(other, FullHouse):
+            if self.set == other.set:
+                return self.full < other.full
+            return self.set < other.set
+        return super()
+
+    def __gt__(self, other):
+        return other.__lt__(self)
+
+    def __eq__(self, other):
+        if isinstance(other, FullHouse):
+            return (self.set == other.set) and (self.full == other.full)
+        return super()
+
+    def __ne__(self, other):
+        return not self.__eq__(self, other)
+
+    def __str__(self):
+        return "Full house, {0}s full of {1}s".format(
+        Card.full_name(self.set),
+        Card.full_name(self.full)
+        )
 
 class Flush(Hand):
     value = 6
